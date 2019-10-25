38e6babd1a66ca2597410d56be9b6df2ebb476b2
Shivani Pandya <sdp43@pitt.edu>
2019-10-25 15:23:12 -0400

    commands to use on terminal for git in the comments

--- exscript4project.R
+++ exscript4project.R
@@ -6,3 +6,9 @@ chap15.1$ppmCaffeine <- factor(chap15.1$ppmCaffeine,
                                levels = c("50", "100", "150", "200"))
 chap15.1Anova <- lm(consumptionDifferenceFromControl ~ ppmCaffeine, data = chap15.1)
 anova(chap15.1Anova)
+
+#what you do in terminal  to add to github
+#git add exscript4project.R
+#git commit exscript4project.R
+#git remote add origin https://github.com/sdp43/GitExample4Project.git
+#git push -u origin master
